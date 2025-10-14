9800312d0cfcc9e7caca739eac729a47c3159a77
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-10-14 18:31:31 +0530

    Add files via upload

---
+++ coreJava/src/day01/Hello.java
@@ -0,0 +1,17 @@
+package day01;
+
+import java.io.PrintStream;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		PrintStream output = System.out;
+		
+		output.println("Hello World");
+		output.print("Hello");
+		output.print("Hello");
+		
+	}
+
+}
