5a9640e15753fe6ae991820640e0763868e285cb
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-10-29 12:31:41 +0530

    Update Wh6.java

--- core_java/src/day04/Wh6.java
+++ core_java/src/day04/Wh6.java
@@ -3,6 +3,27 @@
 
 package day04;
 
-public class Wh6 {
+import java.util.Scanner;
 
+public class Wh6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int revNum = 0;
+        int temp = num;  
+
+        while (num != 0) {
+            int lastDigit = num % 10;    
+            revNum = (revNum * 10) + lastDigit;
+            num = num / 10;   
+        }
+
+        System.out.println("Original number : " + temp);
+        System.out.println("Reversed number : " + revNum);
+
+        sc.close();
+    }
 }
