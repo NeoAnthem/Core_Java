be490eb899746c4c77c79370f554394a0f1ba664
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-11-13 13:15:05 +0530

    Add files via upload

---
+++ core_java/src/day13/Employee.java
@@ -0,0 +1,9 @@
+package day13;
+
+public record Employee(int empId, String name, double salary) {
+
+	
+	public Employee(int empId, String name) {
+		this(empId, name, 0);
+	}
+}
---
+++ core_java/src/day13/polymorphism/RunTimeMain.java
@@ -0,0 +1,25 @@
+package day13.polymorphism;
+
+// Run Time Polymorphism
+public class RunTimeMain {
+
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		
+		cat.walk();
+	}
+}
+
+class Animal{
+	
+	public void walk() {
+		System.out.println("Animal is walking");
+	}
+}
+
+class Cat extends Animal{
+	@Override
+	public void walk() {
+		System.out.println("Cat is running");
+	}
+}
