769e6f004c185f8422c3e088130c96a59c92963a
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-11-18 13:07:25 +0530

    Add files via upload

---
+++ core_java/src/day15/Arr1.java
@@ -0,0 +1,21 @@
+package day15;
+
+public class Arr1 {
+	
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		arr[0] = 50;
+		arr[1] = 10;
+		arr[2] = 6;
+		arr[3] = -15;
+		arr[4] = 25;
+		
+		System.out.println(arr[0]); 
+		System.out.println(arr[1]); 
+		System.out.println(arr[2]); 
+		System.out.println(arr[3]); 
+		System.out.println(arr[4]); 
+		
+	}
+
+}
